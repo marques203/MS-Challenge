027bf0c9073352dffc34915287695881a0366028
Felipe Sampaio <felipe.sampaio.8795@ga.ita.br>
2026-09-03 08:39:42 -0300

    add: order_id - para satisfazer prioridade de chegada em caso de preços iguais

--- matching_engine.py
+++ matching_engine.py
@@ -4,13 +4,19 @@ class MatchingEngine:
     def __init__(self):
         self.buy_orders = []
         self.sell_orders = []
+        self.order_id_counter = 0
 
     def limit_order(self, operation, order):
         if operation == "buy":
+            self.order_id_counter += 1
+            # Invertendo o preço da ordem de compra para que o heapq funcione como uma max-heap
             order[0] = -order[0]
-            heapq.heappush(self.buy_orders, order)
+            order_plus_id = (order[0], self.order_id_counter, order[1])
+            heapq.heappush(self.buy_orders, order_plus_id)
         elif operation == "sell":
-            heapq.heappush(self.sell_orders, order)
+            self.order_id_counter += 1
+            order_plus_id = (order[0], self.order_id_counter, order[1])
+            heapq.heappush(self.sell_orders, order_plus_id)
         else:
             raise ValueError("Operação inválida. Use 'buy' ou 'sell'.")
 
@@ -22,22 +28,22 @@ class MatchingEngine:
                 best_sell = self.sell_orders[0]
                 #Se a quantidade da ordem de compra for maior ou igual a quantidade da melhor ordem de venda,
                 #subtraimos a quantidade da ordem de compra e tiramos a ordem de venda da fila
-                if quantity >= best_sell[1]:
-                    quantity -= best_sell[1]
+                if quantity >= best_sell[2]:
+                    quantity -= best_sell[2]
                     heapq.heappop(self.sell_orders)
                 else:
                     #subtraimos a quantidade da melhor ordem de venda e zeramos a quantidade da ordem agressiva
-                    best_sell[1] -= quantity
+                    best_sell[2] -= quantity
                     quantity = 0
 
         elif operation == "sell":
             while quantity > 0 and self.buy_orders:
                 best_buy = self.buy_orders[0]
-                if quantity >= best_buy[1]:
-                    quantity -= best_buy[1]
+                if quantity >= best_buy[2]:
+                    quantity -= best_buy[2]
                     heapq.heappop(self.buy_orders)
                 else:
-                    best_buy[1] -= quantity
+                    best_buy[2] -= quantity
                     quantity = 0
         else:
             raise ValueError("Operação inválida. Use 'buy' ou 'sell'.")
@@ -50,14 +56,14 @@ class MatchingEngine:
             #Compatibilidade de preços
             if best_buy >= best_sell:
                 #Se a quantidade da ordem de compra for maior que a quantidade da ordem de venda
-                if self.buy_orders[0][1] > self.sell_orders[0][1]:
+                if self.buy_orders[0][2] > self.sell_orders[0][2]:
                     #Executando a ordem de compra contra a ordem de venda
-                    self.buy_orders[0][1] -= self.sell_orders[0][1]
+                    self.buy_orders[0][2] -= self.sell_orders[0][2]
                     heapq.heappop(self.sell_orders)
                 #Se a quantidade da ordem de venda for maior que a quantidade da ordem de compra
-                elif self.buy_orders[0][1] < self.sell_orders[0][1]:
+                elif self.buy_orders[0][2] < self.sell_orders[0][2]:
                     #Executando a ordem de venda contra a ordem de compra
-                    self.sell_orders[0][1] -= self.buy_orders[0][1]
+                    self.sell_orders[0][2] -= self.buy_orders[0][2]
                     heapq.heappop(self.buy_orders)
                 else:
                     #se as quantidades forem iguais, removemos ambas as ordens
