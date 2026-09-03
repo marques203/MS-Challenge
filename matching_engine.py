fa9206cc65f314da379ff28129f8ff12d785e5e1
Felipe Sampaio <felipe.sampaio.8795@ga.ita.br>
2026-09-02 23:36:04 -0300

    add: match_orders funtion - verifica se ha compatibilidade de precos das orders de compra e venda, executando a order seguindo a lógica das quantidades de cada uma para a remocao ou diminuicao da order na priority queue

--- matching_engine.py
+++ matching_engine.py
@@ -14,4 +14,24 @@ class MatchingEngine:
         else:
             raise ValueError("Operação inválida. Use 'buy' ou 'sell'.")
 
-    
+    def match_orders(self):
+        while self.buy_orders and self.sell_orders:
+            #Pegando as melhores ordens de compra e venda
+            best_buy = -self.buy_orders[0][0]
+            best_sell = self.sell_orders[0][0]
+            #Compatibilidade de preços
+            if best_buy >= best_sell:
+                #Se a quantidade da ordem de compra for maior que a quantidade da ordem de venda
+                if self.buy_orders[0][1] > self.sell_orders[0][1]:
+                    #Executando a ordem de compra contra a ordem de venda
+                    self.buy_orders[0][1] -= self.sell_orders[0][1]
+                    heapq.heappop(self.sell_orders)
+                #Se a quantidade da ordem de venda for maior que a quantidade da ordem de compra
+                elif self.buy_orders[0][1] < self.sell_orders[0][1]:
+                    #Executando a ordem de venda contra a ordem de compra
+                    self.sell_orders[0][1] -= self.buy_orders[0][1]
+                    heapq.heappop(self.buy_orders)
+                else:
+                    #se as quantidades forem iguais, removemos ambas as ordens
+                    heapq.heappop(self.buy_orders)
+                    heapq.heappop(self.sell_orders)
